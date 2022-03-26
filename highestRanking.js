9f23252bb7b34d15a494caaa2080878f9bbad422
tsumitakaoru <k.tsumita01@gmail.com>
2022-03-26 19:22:21 -0400

    Add highestRanking.js

---
+++ highestRanking.js
@@ -0,0 +1,4 @@
+// Write a program in C# or JavaScript that determines the highest-ranking of 2 regular 5-card Poker hands. The program should accomplish the following 2 points:
+// ·         Provide a way to input the 5 cards in each of the 2 hands
+// ·         Analyze and display the winning hand 
+// If using C#, the program should compile and run on Windows 7+ (any version of the .Net framework). If using JavaScript, the program should run in either Chrome or Firefox.
