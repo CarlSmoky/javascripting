9958020e2a50d32acfbc50e85604fe1208eafc26
tsumitakaoru <k.tsumita01@gmail.com>
2022-04-13 08:59:39 -0400

    Change v

--- questionsMarks.js
+++ questionsMarks.js
@@ -5,7 +5,12 @@ const QuestionsMarks = str => {
   for (let i = 0; i < str.length; i++) {
     for (let j = i + 1; j < str.length; j++) {
       if (Number(str[i]) + Number(str[j]) === 10) {
+        console.log("str.i:", str[i], "str.j:", str[j]);
+        console.log("i:", i, "j:", j);
         result = true;
+        console.log("slice: ", str.slice(i, j));
+        console.log("split: ", str.slice(i, j).split("?"));
+        console.log("length:", str.slice(i, j).split("?").length);
         if (str.slice(i, j).split("?").length - 1 < 3) {
           return false;
         }
