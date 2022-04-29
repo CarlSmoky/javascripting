018a9abbf7aee16a5e60b907a36cb9e0132b73a5
tsumitakaoru <k.tsumita01@gmail.com>
2022-04-29 11:16:13 -0400

    Add nthOfLetter.js

---
+++ nthOfLetter.js
@@ -0,0 +1,6 @@
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
+const myFunction = (a, n) => a[n - 1];
+
+
+console.log(myFunction('abcd',1));
