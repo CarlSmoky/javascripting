3d374f70bf4ac867a021fb6682ab4ad149a6f972
tsumitakaoru <k.tsumita01@gmail.com>
2022-04-29 11:40:46 -0400

    Add lastThreeCaracters.js

---
+++ lastThreeCaracters.js
@@ -0,0 +1,8 @@
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+const myFunction = str => {
+  return str.slice(str.length - 3);
+};
+
+myFunction('abcdefg');
